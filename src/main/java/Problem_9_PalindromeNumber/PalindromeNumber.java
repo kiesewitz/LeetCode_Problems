ba7fe8df9a89001fcb719d0c0de44693951ee567
SchaarNikita <211wita20@o365.htl-leoben.at>
2026-01-29 21:37:11 +0100

    Add problem 9 solution (not finished)

---
+++ src/main/java/Problem_9_PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,19 @@
+package Problem_9_PalindromeNumber;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(55));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x >= 0) {
+            try {
+                int y = Integer.
+                System.out.println(y);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
